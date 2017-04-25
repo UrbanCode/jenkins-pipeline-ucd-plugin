fffceba44531c22d9c4ea724856344e1bc6ca4ff
Goroff, Mitch <Mitchell.Goroff@fmr.com>
2017-04-25 14:20:24 -0400

    Reverted to previous revision. Removed changing "/" to "&" on property sheet path

--- src/main/java/com/urbancode/jenkins/plugins/ucdeploy/VersionHelper.java
+++ src/main/java/com/urbancode/jenkins/plugins/ucdeploy/VersionHelper.java
@@ -295,8 +295,6 @@ public class VersionHelper {
                 propSheetDef = compClient.getComponentVersionPropSheetDef(component);
                 propSheetDefId = (String) propSheetDef.get("id");
                 propSheetDefPath = (String) propSheetDef.get("path");
-                propSheetDefPath = propSheetDefPath.replaceAll("/", "&");
-                listener.getLogger().println("Property Sheet Path is " + propSheetDefPath);
                 existingPropDefJsonArray = propClient.getPropSheetDefPropDefs(propSheetDefPath);
             }
             catch (IOException ex) {
